996287285e5d636383cb92b59fa77af087c6866a
Adam Fairhead <adam@adamfairhead.com>
2016-08-09 17:04:49 -0400

    Add modal

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: [
+          'js/components/*.js'
+        ],
+        dest: 'js/production.js',
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/production.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
---
+++ js/components/home.js
@@ -0,0 +1,8 @@
+jQuery(function($) {
+
+  $(document).on('click', '.the313-open', function() {
+    $('.the313-modal').addClass('visible');
+    return false;
+  });
+
+});
